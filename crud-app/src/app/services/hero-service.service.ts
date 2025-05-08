0b94c41b0184a8252eb124b11f814542d7c81d9e
kvandana451 <kvandana451@gmail.com>
2025-05-09 02:28:45 +0530

    Get heroes with Http Client

--- crud-app/src/app/services/hero-service.service.ts
+++ crud-app/src/app/services/hero-service.service.ts
@@ -1,11 +1,18 @@
+import { Hero } from '../interfaces/hero';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HeroServiceService {
+export class HeroService {
   constructor(private http: HttpClient) {}
   // Define the heroesUrl of the form :base/:collectionName with the address of the heroes resource on the server. Here base is the resource to which requests are made, and collectionName is the heroes data object in the in-memory-data-service.ts.
   private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    // HttpClient.get() returns the body of the response as an untyped JSON object by default. Applying the optional type specifier, <Hero[]> , adds TypeScript capabilities, which reduce errors during compile time.
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
 }
