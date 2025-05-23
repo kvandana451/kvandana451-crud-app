76556bfcfc2031a8f699f9e28ded461369ed74ce
kvandana451 <kvandana451@gmail.com>
2025-05-23 20:49:02 +0530

    css for hero search component

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeroService } from './services/hero-service.service';
 import { Hero } from './interfaces/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'crud-app';
+  title = 'Tour of Heroes';
   constructor() {}
   ngOnInit() {}
 }
--- crud-app/src/app/components/dashboard/dashboard.component.ts
+++ crud-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { HeroService } from '../../services/hero-service.service';
+import { Hero } from '../../interfaces/hero';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeroSearchComponent],
+  imports: [HeroSearchComponent, RouterLink, NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  topHeroes?: Hero[];
+  constructor(private heroService: HeroService) {}
+  ngOnInit() {
+    this.heroService.getHeroes().subscribe((data) => {
+      this.topHeroes = data.slice(0, 5);
+    });
+  }
+}
